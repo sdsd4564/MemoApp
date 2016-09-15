f98b15e9a208241c809db5e5f64231578870e204
한재현 <sdsd4564@gmail.com>
2016-09-15 20:56:28 +0900

    2016 09 14 (3) - 1 수정

--- app/src/main/java/hanbat/encho/com/clipboardmake/MemoService.java
+++ app/src/main/java/hanbat/encho/com/clipboardmake/MemoService.java
@@ -6,6 +6,7 @@ import android.content.ClipDescription;
 import android.content.ClipboardManager;
 import android.content.Intent;
 import android.database.Cursor;
+import android.os.Build;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.util.Log;
@@ -34,11 +35,16 @@ public class MemoService extends Service {
             public void onPrimaryClipChanged() {
                 data = manager.getPrimaryClip();
                 Log.d(TAG, data.getDescription().getMimeType(0));
-                if (!manager.getPrimaryClipDescription().hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN)) {
-                    Toast.makeText(Application.getMyContext(), "텍스트가 아니야 !", Toast.LENGTH_SHORT).show();
-                } else {
+                if (manager.getPrimaryClipDescription().hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN)) {
                     mOpenner.insertColumn(data.getItemAt(0).getText().toString());
                     Toast.makeText(Application.getMyContext(), "메모에 추가되었습니다", Toast.LENGTH_SHORT).show();
+                } else if(manager.getPrimaryClipDescription().hasMimeType(ClipDescription.MIMETYPE_TEXT_HTML)) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                        mOpenner.insertColumn(data.getItemAt(0).getHtmlText());
+                        Toast.makeText(Application.getMyContext(), "메모에 추가되었습니다", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(Application.getMyContext(), "텍스트가 아니야 !", Toast.LENGTH_SHORT).show();
                 }
             }
         });
